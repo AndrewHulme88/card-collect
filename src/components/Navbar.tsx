ae131e3221898d1a5c3460e3ed5c2cbae73c76f5
Andrew <andrew_hulme04@hotmail.com>
2025-10-31 19:37:36 +1100

    Added styling to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import '../styles/navbar.css'
 
 const Navbar: React.FC = () => {
     return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-            </ul>
+        <nav className='nav-bar'>
+            <div className='navbar-left'>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                        <Link to="/cards">Cards</Link>
+                    </li>
+                </ul>
+            </div>
         </nav>
     );
 };
